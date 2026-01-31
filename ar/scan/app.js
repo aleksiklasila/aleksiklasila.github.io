e3d94fdf28ce966ee1d687497da68716c18cfcdb
aleksiklasila <aleksi.klasila35@gmail.com>
2026-01-31 21:43:06 +0200

    edit

--- ar/scan/app.js
+++ ar/scan/app.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { GLTFExporter } from 'three/addons/exporters/GLTFExporter.js';
-import { buildMeshFromDepth } from './ar-mesh.js?v=111';
-import { ARScanner } from './ar-scanner.js?v=111';
+import { buildMeshFromDepth } from './ar-mesh.js?v=112';
+import { ARScanner } from './ar-scanner.js?v=112';
 
 // --- Constants & Globals ---
 let camera, scene, renderer;
