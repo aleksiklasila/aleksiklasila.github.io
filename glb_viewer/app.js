565d70f177583a842b456d06c87763634048e34f
aleksiklasila <aleksi.klasila35@gmail.com>
2026-02-15 14:29:44 +0200

    edit

--- glb_viewer/app.js
+++ glb_viewer/app.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { TransformControls } from 'three/addons/controls/TransformControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 
 // --- Global State ---
 const state = {
